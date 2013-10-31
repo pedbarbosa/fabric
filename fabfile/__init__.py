4d0ff4fe198a53f3a3c61f71026a8e1be38cb54d
Pedro Barbosa <pedbarbosa@gmail.com>
2013-10-31 10:31:46 +1000

    Added hosts configuration

--- fabfile/__init__.py
+++ fabfile/__init__.py
@@ -4,5 +4,9 @@ from say import *
 from sshkey import *
 from yum import *
 
+# Import host configuration
+from hosts import *
+
 # Set Fabric to use SSH configurations from .ssh/config
+env.skip_bad_hosts = True
 env.use_ssh_config = True
